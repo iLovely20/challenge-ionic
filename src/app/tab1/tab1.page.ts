96dd0ea927a41c14d0f32acb8617d6c5caeb944f
ilovely20 <ghiirawan10@gmail.com>
2023-06-10 19:02:24 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+declare var window: any;
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,67 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  nama: string = '';
+  nim: string = '';
+  kelas: string = '';
+  tanggalLahir: string = '';
+  alamat: string = '';
+  golonganDarah: string = '';
+
+  formData={
+    nama:'',
+    nim:'',
+    kelas: '',
+    tanggalLahir: '',
+    alamat: '',
+    golonganDarah:'',
+  }
+
+  constructor(
+    private toastController: ToastController,
+  ) {}
+
+  sendWhatsAppMessage() {
+    const message = `
+      Nama: ${this.nama}
+      NIM: ${this.nim}
+      Kelas: ${this.kelas}
+      Tanggal Lahir: ${this.tanggalLahir}
+      Alamat: ${this.alamat}
+      Golongan Darah: ${this.golonganDarah}
+    `;
+
+    const encodedMessage = encodeURIComponent(message);
+    const phoneNumber = '089502657747';
+
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    window.open(url, '_system');
+  }
+
+  sendTelegramMessage() {
+    const message = `
+      Nama: ${this.nama}
+      NIM: ${this.nim}
+      Kelas: ${this.kelas}
+      Tanggal Lahir: ${this.tanggalLahir}
+      Alamat: ${this.alamat}
+      Golongan Darah: ${this.golonganDarah}
+    `;
+  
+    const encodedMessage = encodeURIComponent(message);
+    const username = '@OhDearLovely';
+  
+    const url = `tg://msg?text=${encodedMessage}&to=${username}`;
+  
+    window.location.href = url;
+  }
+
+
+  
+
+  ngOnInit() {
+  }
+
 
 }
